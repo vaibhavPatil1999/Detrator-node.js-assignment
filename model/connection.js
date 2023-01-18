3d412e7d40384be7adae79dbbf9c9d1e81a582c8
vaibhavPatil <vaibhavpatillf02@gmail.com>
2023-01-18 10:41:44 +0530

    ok

--- model/connection.js
+++ model/connection.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql')
+const mysql = require("mysql");
 const connection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:process.env.DB_PASSWORD,
-    database:'test_db'
-})
-const query = "create table posts (id int AUTO_INCREMENT , user_id int , filename varchar(255) , PRIMARY_KEY(id))"
-
-module.exports = connection
+  host: "localhost",
+  user: "root",
+  password: process.env.DB_PASSWORD,
+  database: "test_db",
+});
+module.exports = connection;
